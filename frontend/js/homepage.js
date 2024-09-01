766dcb37c64dd94a5625e9dd8592b5cd782ecd82
KedarKK1 <officialkedark1@gmail.com>
2024-09-01 17:08:22 +0530

    frontend done, design changes implemented

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -13,3 +13,23 @@ window.addEventListener('load', function() {
         document.getElementById('myloader').style.display = 'none';
     }, 3000); // 3 seconds
 });
+
+let btnOne = document.getElementsByClassName("btnOne");
+let btnTwo = document.getElementsByClassName("btnTwo");
+let btnThree = document.getElementsByClassName("btnThree");
+
+function myFunction() {
+    document.getElementById("imgTobeSet").setAttribute("src", "./assets/img/481602-PH02WC-347.jpg");
+  }
+
+  function myFunction2() {
+    document.getElementById("imgTobeSet").setAttribute("src", "./assets/img/4214796.jpg");
+  }
+
+  function myFunction3() {
+    document.getElementById("imgTobeSet").setAttribute("src", "assets/img/6895861.jpg");
+  }
+
+document.getElementById("btnOne").addEventListener("click", myFunction);
+document.getElementById("btnTwo").addEventListener("click", myFunction2);
+document.getElementById("btnThree").addEventListener("click", myFunction3);
